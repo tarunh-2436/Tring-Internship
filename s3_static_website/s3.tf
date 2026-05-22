bf9fc6dbbe2f756da4d573562a444599c7efd8fd
tarunh-2436 <tarunharish2436@gmail.com>
2026-05-22 17:44:35 +0530

    added missing file

---
+++ s3_static_website/s3.tf
@@ -0,0 +1,56 @@
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.static_site
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+
+        Action = [
+          "s3:GetObject"
+        ]
+
+        Resource = [
+          "${aws_s3_bucket.static_site.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.static_site.id
+  key          = "index.html"
+  source       = "website/index.html"
+  content_type = "text/html"
+}
