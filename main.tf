8ccce94ad9d1aaf06c3ea5215ca7ac1477eaf98d
tarunh-2436 <tarunharish2436@gmail.com>
2026-05-22 11:33:18 +0530

    Wrote terraform file to create iam user that can list/view all users

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
@@ -11,4 +11,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws-iam-user" "test"
+# Create IAM User
+resource "aws_iam_user" "test1" {
+  name = "TerraformIAMTest1"
+}
+
+# Create Access Key
+resource "aws_iam_access_key" "test1" {
+  user = aws_iam_user.test1.name
+}
+
+# Create IAM Policy Document
+data "aws_iam_policy_document" "test1_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "iam:ListUsers"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+# Attach Policy to User
+resource "aws_iam_user_policy" "test1_policy" {
+  name = "TerraformTest"
+
+  user = aws_iam_user.test1.name
+
+  policy = data.aws_iam_policy_document.test1_policy.json
+}
