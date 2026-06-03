9e8053cce713b145b358469f4aafe0457118522f
Tarun Sivaraj Harish <tarunharish2436@gmail.com>
2026-06-03 15:16:17 +0530

    Implemented frontend webpage and connected backend

---
+++ website/scripts.js
@@ -0,0 +1,85 @@
+const API_URL =
+  "https://sa2mb6latf.execute-api.us-east-1.amazonaws.com/prod/feedback";
+
+async function submitFeedback() {
+  const name = document.getElementById("name").value;
+  const feedback = document.getElementById("feedback").value;
+
+  console.log("Submitting feedback:", { name, feedback });
+
+  if (!name || !feedback) {
+    document.getElementById("message").innerHTML = "Please fill all fields";
+
+    return;
+  }
+  try {
+    const response = await fetch(API_URL, {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        name: name,
+        feedback: feedback,
+      }),
+    });
+
+    const result = await response.json();
+    console.log("Feedback submission result:", result);
+
+    if (response.ok) {
+      document.getElementById("message").innerHTML = result.message;
+
+      document.getElementById("name").value = "";
+      document.getElementById("feedback").value = "";
+    } else {
+      document.getElementById("message").innerHTML = result.message;
+    }
+  } catch (error) {
+    console.error("Error submitting feedback:", error);
+
+    document.getElementById("message").innerHTML =
+      "Unable to connect to server.";
+  }
+}
+
+async function fetchFeedback() {
+  try {
+    const response = await fetch(API_URL);
+    
+    if (!response.ok) {
+    const result = await response.json();
+
+    document.getElementById("message").textContent =
+        result.message;
+
+    return;
+    }
+
+    const feedbacks = await response.json();
+
+    const feedbackJson = JSON.stringify(feedbacks, null, 4);
+
+    const blob = new Blob([feedbackJson], {
+      type: "application/json",
+    });
+
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+
+    link.href = url;
+
+    link.download = "feedbacks.json";
+
+    link.click();
+
+    URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error("Error fetching feedback:", error);
+    document.getElementById("message").innerHTML =
+      "Unable to connect to server.";
+  }
+}
